4671c2daca7b09200faac918d15cdf6ec73735ee
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-04-23 17:00:13 +0530

    add user handler

--- internal/user/handler.go
+++ internal/user/handler.go
@@ -1,1 +1,37 @@
 package user
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type UserHandler struct {
+	groupName   string
+	userService UserService
+}
+
+func NewUserHandlerFrom(userService UserService) *UserHandler {
+	return &UserHandler{"api/user", userService}
+}
+
+func (handler *UserHandler) RegisterEndpoints(r *gin.Engine) {
+	userGroup := r.Group(handler.groupName)
+
+	userGroup.POST("", handler.CreateUser)
+}
+
+func (handler *UserHandler) CreateUser(ctx *gin.Context) {
+	userData := NewInputUser()
+	err := ctx.BindJSON(&userData)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"err": "failed to bind user data"})
+		return
+	}
+	newUser, err := handler.userService.Create(userData)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"err": "failed to create new user"})
+		return
+	}
+	ctx.JSON(http.StatusCreated, gin.H{"msg": "user created successfully", "data": newUser})
+}
