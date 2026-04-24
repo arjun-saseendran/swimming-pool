d5f75123c4c3ecde27af10ef925201ab51ddc8ef
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-04-24 15:45:20 +0530

    add booking handler

--- internal/booking/handler.go
+++ internal/booking/handler.go
@@ -1,1 +1,38 @@
 package booking
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type BookingHandler struct {
+	groupName      string
+	bookingService BookingService
+}
+
+func NewUserHandlerFrom(bookingService BookingService) *BookingHandler {
+	return &BookingHandler{"api/booking", bookingService}
+}
+
+func (handler *BookingHandler)RegisterEndPoints(r *gin.Engine){
+	bookingGroup := r.Group(handler.groupName)
+
+	bookingGroup.POST("",handler.CreateBooking)
+	}
+
+func (handler *BookingHandler) CreateBooking(ctx *gin.Context){
+	bookingData := NewInputBooking()
+	err := ctx.BindJSON(&bookingData)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"err":"failed bind booking data"})
+	return
+	}
+
+	newBooking, err := handler.bookingService.Create(bookingData)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"err":"failed to create new booking"})
+		return
+	}
+	ctx.JSON(http.StatusCreated, gin.H{"msg":"booking created successfully", "data":newBooking})
+}
