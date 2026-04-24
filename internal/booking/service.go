af2b7f07237992a88f833772210fa9690458b12b
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-04-24 15:00:25 +0530

    add booking service

--- internal/booking/service.go
+++ internal/booking/service.go
@@ -1,1 +1,22 @@
 package booking
+
+import "gorm.io/gorm"
+
+type BookingService interface {
+	Create(bookingData *InputBooking) (*Booking, error)
+}
+
+type bookingService struct{ db *gorm.DB }
+
+func NewBookingService(db *gorm.DB) BookingService {
+	return &bookingService{db: db}
+}
+
+func (bs *bookingService) Create(bookingData *InputBooking) (*Booking, error) {
+	newBooking := &Booking{Title: bookingData.Title, UserID: bookingData.UserID}
+	result := bs.db.Create(newBooking)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return newBooking, nil
+}
